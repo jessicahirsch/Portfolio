e8477b821c0fdcf5813d71ae898f99da366e3f56
Jessica Hirsch <jrachelh5@gmail.com>
2017-05-31 16:21:46 -0400

    Scroll Function

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // console.log('ready');
 
-$('body').videoBG({
-    position:"fixed",
-    zIndex:-1,
-    mp4:'christmas_snow.mp4',
-    ogv:'christmas_snow.ogv',
-    webm:'christmas_snow.webm',
-    poster:'christmas_snow.jpg',
-    opacity:0.5
-  });
+// $('body').videoBG({
+//     position:"fixed",
+//     zIndex:-1,
+//     mp4:'christmas_snow.mp4',
+//     ogv:'christmas_snow.ogv',
+//     webm:'christmas_snow.webm',
+//     poster:'christmas_snow.jpg',
+//     opacity:0.5
+//   });
+
+$(document).ready(function(){
+   $(".anchorClick").click(function(){
+      $('html, body').animate({
+       scrollTop: $($(this).attr('href')).offset().top
+      }, 2000);
+   });
+});
