b7b19c3a2941ed91574cc5bd485697d68836d15c
Jessica Hirsch <jrachelh5@gmail.com>
2017-05-31 15:36:48 -0400

    Working Background Video

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log('ready');
+
+$('body').videoBG({
+    position:"fixed",
+    zIndex:-1,
+    mp4:'christmas_snow.mp4',
+    ogv:'christmas_snow.ogv',
+    webm:'christmas_snow.webm',
+    poster:'christmas_snow.jpg',
+    opacity:0.5
+  });
